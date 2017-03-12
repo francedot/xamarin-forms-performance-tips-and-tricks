f9b7630d9f05d78dea62635a018bf43e7f51f422
francedot <francibon93@gmail.com>
2017-03-12 16:55:01 +0100

    Add AppDemo 1 - Binding Sample

---
+++ Performance/AppDemo1/AppDemo1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppDemo1
+{
+    public partial class MainPage : ContentPage
+    {
+        readonly MainPageViewModel _viewModel;
+
+        public MainPage()
+        {
+            this.BindingContext = _viewModel = new MainPageViewModel();
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await _viewModel.OnAppearingAsync();
+        }
+    }
+}
---
+++ Performance/AppDemo1/AppDemo1/MainPageViewModel.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using AppDemo1.Annotations;
+using Xamarin.Forms;
+
+namespace AppDemo1
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string _value1;
+        string _value2;
+        string _value3;
+        string _value4;
+        string _value5;
+        string _value6;
+        string _value7;
+        string _value8;
+        string _value9;
+        string _value10;
+        string _value11;
+        string _value12;
+        string _value13;
+        string _value14;
+        string _value15;
+        string _value16;
+        string _value17;
+        string _value18;
+        string _value19;
+        string _value20;
+        string _value21;
+        string _value22;
+        string _value23;
+        string _value24;
+        string _value25;
+        string _value26;
+        string _value27;
+        string _value28;
+        string _value29;
+        string _value30;
+        string _value31;
+        string _value32;
+
+        public string Value1
+        {
+            get { return _value1; }
+            set
+            {
+                _value1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value2
+        {
+            get { return _value2; }
+            set
+            {
+                _value2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value3
+        {
+            get { return _value3; }
+            set
+            {
+                _value3 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value4
+        {
+            get { return _value4; }
+            set
+            {
+                _value4 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value5
+        {
+            get { return _value5; }
+            set
+            {
+                _value5 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value6
+        {
+            get { return _value6; }
+            set
+            {
+                _value6 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value7
+        {
+            get { return _value7; }
+            set
+            {
+                _value7 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value8
+        {
+            get { return _value8; }
+            set
+            {
+                _value8 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value9
+        {
+            get { return _value9; }
+            set
+            {
+                _value9 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value10
+        {
+            get { return _value10; }
+            set
+            {
+                _value10 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value11
+        {
+            get { return _value11; }
+            set
+            {
+                _value11 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value12
+        {
+            get { return _value12; }
+            set
+            {
+                _value12 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value13
+        {
+            get { return _value13; }
+            set
+            {
+                _value13 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value14
+        {
+            get { return _value14; }
+            set
+            {
+                _value14 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value15
+        {
+            get { return _value15; }
+            set
+            {
+                _value15 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value16
+        {
+            get { return _value16; }
+            set
+            {
+                _value16 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value17
+        {
+            get { return _value17; }
+            set
+            {
+                _value17 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value18
+        {
+            get { return _value18; }
+            set
+            {
+                _value18 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value19
+        {
+            get { return _value19; }
+            set
+            {
+                _value19 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value20
+        {
+            get { return _value20; }
+            set
+            {
+                _value20 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value21
+        {
+            get { return _value21; }
+            set
+            {
+                _value21 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value22
+        {
+            get { return _value22; }
+            set
+            {
+                _value22 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value23
+        {
+            get { return _value23; }
+            set
+            {
+                _value23 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value24
+        {
+            get { return _value24; }
+            set
+            {
+                _value24 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value25
+        {
+            get { return _value25; }
+            set
+            {
+                _value25 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value26
+        {
+            get { return _value26; }
+            set
+            {
+                _value26 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value27
+        {
+            get { return _value27; }
+            set
+            {
+                _value27 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value28
+        {
+            get { return _value28; }
+            set
+            {
+                _value28 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value29
+        {
+            get { return _value29; }
+            set
+            {
+                _value29 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value30
+        {
+            get { return _value30; }
+            set
+            {
+                _value30 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value31
+        {
+            get { return _value31; }
+            set
+            {
+                _value31 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value32
+        {
+            get { return _value32; }
+            set
+            {
+                _value32 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task OnAppearingAsync()
+        {
+            Value1 = "Hello Codemotion";
+            Value2 = "Hello Codemotion";
+            Value3 = "Hello Codemotion";
+            Value4 = "Hello Codemotion";
+            Value5 = "Hello Codemotion";
+            Value6 = "Hello Codemotion";
+            Value7 = "Hello Codemotion";
+            Value8 = "Hello Codemotion";
+            Value9 = "Hello Codemotion";
+            Value10 = "Hello Codemotion";
+            Value11 = "Hello Codemotion";
+            Value12 = "Hello Codemotion";
+            Value13 = "Hello Codemotion";
+            Value14 = "Hello Codemotion";
+            Value15 = "Hello Codemotion";
+            Value16 = "Hello Codemotion";
+            Value17 = "Hello Codemotion";
+            Value18 = "Hello Codemotion";
+            Value19 = "Hello Codemotion";
+            Value20 = "Hello Codemotion";
+            Value21 = "Hello Codemotion";
+            Value22 = "Hello Codemotion";
+            Value23 = "Hello Codemotion";
+            Value24 = "Hello Codemotion";
+            Value25 = "Hello Codemotion";
+            Value26 = "Hello Codemotion";
+            Value27 = "Hello Codemotion";
+            Value28 = "Hello Codemotion";
+            Value29 = "Hello Codemotion";
+            Value30 = "Hello Codemotion";
+            Value31 = "Hello Codemotion";
+            Value32 = "Hello Codemotion";
+
+            await Task.Delay(TimeSpan.FromSeconds(3)); 
+            FormsProfiler.DumpStats();
+        }
+    }
+}
