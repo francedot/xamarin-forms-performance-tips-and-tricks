8658a2f612448897ad0638d45f69dd0d22666ea0
francedot <francibon93@gmail.com>
2017-03-17 23:48:59 +0100

    Add AppDemo0 - XamlC

---
+++ Performance/AppDemo0/AppDemo0/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppDemo0
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var watch = Stopwatch.StartNew();
+            InitializeComponent();
+            watch.Stop();
+            Debug.WriteLine("Elapsed: " + watch.ElapsedMilliseconds + "ms");
+        }
+    }
+}
